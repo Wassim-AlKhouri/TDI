ab09fcc2edfe1bae8b2a3adcb4c06b6fe35c13df
Bryven7 <102870435+Bryven7@users.noreply.github.com>
2022-04-10 20:20:39 +0200

    Create class Cell

---
+++ app/src/main/java/com/example/myapplication/Cell.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.graphics.RectF
+
+class Cell (coordX : Int, coordY : Int, step: Float) {
+    val R = RectF(coordX,coordY*step)
+    var cellWidth = 0
+    var cellHeight = 0
+
+
+
+
+}
